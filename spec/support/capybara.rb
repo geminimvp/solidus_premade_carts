b4bb6638e33d622e5e8dc431fa63aba88a3c78d9
fastjames <fastjames@gmail.com>
2019-01-09 16:12:22 -0600

    Require chromedriver-helper in specs
    This is needed to allow chromedriver-helper to work properly.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,7 @@
 require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
+require 'selenium/webdriver'
+require 'chromedriver-helper'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods, type: :requests
