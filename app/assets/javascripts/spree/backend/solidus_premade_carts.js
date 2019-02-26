a8d0e677f117abcbdac9f1fbddd42c3758171548
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-02-26 11:07:28 -0600

    Fix issue not copying URL to clipboard

--- app/assets/javascripts/spree/backend/solidus_premade_carts.js
+++ app/assets/javascripts/spree/backend/solidus_premade_carts.js
@@ -1,20 +1,19 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
 
-$(document).ready(function(){
+Spree.ready(function() {
   $('#copy-to-clipboard').on('click', function() {
-    $('#premade-cart-text').select()
-    document.execCommand("copy")
-    flash = $(`<div class='flash success'>Copied the text: ${$('#premade-cart-text').get(0).value}</div>`)
-      .appendTo('.js-flash-wrapper')
-      .on('click', function() {
-        $(this).remove();
-      });
+    const url = $('#premade-cart-link').text();
+
+    show_flash('success', `Copied the text: ${url}`);
+    navigator.clipboard.writeText(url);
   })
 
   $('#add_variant_id').on('change', function(event) {
-    $('#add-variant-to-cart').removeClass('d-none')
-    paramIndicator = window.location.search.length > 0 ? '&' : '?'
-    $('#add-variant-to-cart').attr('href', `${window.location.href}${paramIndicator}variant_ids[]=${event.added.id}`)
+    const paramIndicator = window.location.search.length > 0 ? '&' : '?';
+    const url = `${window.location.href}${paramIndicator}variant_ids[]=${event.added.id}`;
+
+    $('#add-variant-to-cart').removeClass('d-none');
+    $('#add-variant-to-cart').attr('href', `${url}`)
   })
 })
