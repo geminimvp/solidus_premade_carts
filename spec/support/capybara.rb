0366cfee9696d55a6f79819803662ab4c1578b70
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-02-28 15:34:16 -0600

    Update capybara configuration

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,13 +1,19 @@
 require 'capybara/rspec'
-require 'capybara-screenshot/rspec'
+require 'capybara/rails'
 require 'selenium/webdriver'
-require 'chromedriver-helper'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods, type: :requests
 
-  Capybara.javascript_driver = :selenium
-  Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    browser_options = ::Selenium::WebDriver::Chrome::Options.new
+    browser_options.args << '--headless'
+    browser_options.args << '--disable-gpu'
+    browser_options.args << '--window-size=1440,1080'
+
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
   end
+
+  Capybara.javascript_driver = :selenium_chrome_headless
+  Capybara.default_max_wait_time = 10
 end
