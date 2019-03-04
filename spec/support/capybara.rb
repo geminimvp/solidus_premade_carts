7ad69faa7f6ca991e10a820e841560434affcd10
fastjames <fastjames@gmail.com>
2019-03-04 10:39:36 -0600

    Pin sqlite3, chromedriver versions
    To maintain compatibility, sqlite3 needs to be pinned to 1.3.x. Also, we
    add the config required to run selenium headless and pin chromedriver to
    maintain compatibility with CircleCI.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,8 +6,20 @@ require 'chromedriver-helper'
 RSpec.configure do |config|
   config.include Rack::Test::Methods, type: :requests
 
+  Capybara.server_port = 8888 + ENV['TEST_ENV_NUMBER'].to_i
+
+  Chromedriver.set_version '2.45'
+
   Capybara.javascript_driver = :selenium
   Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+        chromeOptions: { args: %w[ headless disable-gpu window-size=1280,1024 ] }
+      )
+    )
   end
+
+
 end
